33cb76454ea6e07a92097b5df7769b254a744634
Anna Runiewicz <annaruniewicz@op.pl>
2019-09-23 22:43:55 +0200

    drugi commit

---
+++ src/Main1.java
@@ -0,0 +1,22 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+	//Ta zmienna przechowuje typ int
+	int int1 = 1;
+	System.out.println(int1);
+	//Ta zmienna przechowuje typ char
+	char char2 = 'n';
+	System.out.println(char2);
+	//Ta zmienna przechowuje typ long
+	long long3 = 104;
+	System.out.println(long3);
+	//Ta zmienna przechowuje typ double
+	double doubleVariable4 = 15.1234;
+	System.out.println(doubleVariable4);
+	boolean boolFalse5 = false;
+	//Ta zmienna przechowuje typ boolean
+	System.out.println(boolFalse5);
+	}
+
+}
